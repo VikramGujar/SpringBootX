7b256c70fc0c7864cbaa31f57f592c7a7c6a4891
VikramGujar <vikramgujar300@gmail.com>
2025-06-09 10:56:37 +0530

    Request forwarding

---
+++ MVCDataRenderingForwardRequest/src/main/java/com/vik/MvcDataRenderingForwardRequestApplication.java
@@ -0,0 +1,13 @@
+package com.vik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDataRenderingForwardRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcDataRenderingForwardRequestApplication.class, args);
+	}
+
+}
---
+++ MVCDataRenderingForwardRequest/src/main/java/com/vik/controller/RequestForwarding.java
@@ -0,0 +1,5 @@
+package com.vik.controller;
+
+public class RequestForwarding {
+
+}
