6cb6d91471566bb0b2e1f8f631ffe1300df0d94b
VikramGujar <vikramgujar300@gmail.com>
2025-06-09 11:12:02 +0530

    Forwading request from one handler method to another handler method

--- MVCDataRenderingForwardRequest/src/main/java/com/vik/controller/RequestForwarding.java
+++ MVCDataRenderingForwardRequest/src/main/java/com/vik/controller/RequestForwarding.java
@@ -1,5 +1,24 @@
 package com.vik.controller;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RequestForwarding {
 
+	@RequestMapping("/")
+	public String methodOne( Map<String, Object> map)
+	{
+		System.out.println(map.getClass());
+		return "forward:home";
+	}
+	
+	@RequestMapping("/home")
+	public String methodTwo()
+	{
+		
+		return "home";
+	}
 }
---
+++ MVCDataRenderingForwardRequest/src/main/webapp/WEB-INF/pages/home.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" isELIgnored="false" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1 style="font: bold; color: orange; text-align: center;">Welcome To Java</h1>
+</body>
+</html>
